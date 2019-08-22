57b28844a31c1e963c76c6ea362f8b23eafae536
Nick Palladinos <nick.palladinos@gmail.com>
2019-08-22 18:28:38 +0300

    Update

--- CircuitSynth/Scratch.fsx
+++ CircuitSynth/Scratch.fsx
@@ -48,7 +48,7 @@ let numOfTries = 1
 let numOfOps = opExprs.Length
 let numOfInstrsIndex = 1
 let numOfSamples = 1
-let numOfVars = 5
+let numOfVars = 3
 let final = int (2.0 ** (float numOfVars))
 
 let xors = (fun i -> xors <| toBits' numOfVars i)
@@ -85,9 +85,10 @@ let population : (int -> bool) -> Ops -> BoolExpr' [] [] = fun f opStruct ->
 
 
 let randomSubExprs : BoolExpr' [] [] -> BoolExpr' [] [] = fun exprs -> 
-    [| for expr in exprs  do yield Seq.initInfinite id |> Seq.map (fun _ -> tryWith (fun () -> rndBoolExpr (rand.Next(3, expr.Length)) expr |> updateVars) [||]) |> Seq.distinct |> Seq.take expr.Length |] 
+    [| for expr in exprs  do yield Seq.initInfinite id |> Seq.map (fun _ -> tryWith (fun () -> rndBoolExpr (rand.Next(2, expr.Length)) expr |> updateVars) [||]) |> take' 100 |] 
     |> Seq.concat
     |> Seq.filter (fun expr -> expr.Length > 1)
+    |> Seq.distinct
     |> Seq.toArray
 
 let matches : Ops -> BoolExpr' [] [] -> (int * int * BoolExpr' []) [] = fun opStruct exprs -> 
@@ -116,7 +117,7 @@ let updateOps : BoolExpr' [] [] -> Ops -> Ops = fun exprs ops ->
                                    { OpExprs = Array.append ops'.OpExprs [|toBoolExpr ops.OpExprs expr|] ;
                                      Ops = Array.append ops'.Ops [|eval' ops.Ops expr|] ;
                                      OpStrs = Array.append ops'.OpStrs [|toOpStr i|] ;
-                                     ArityOps = Array.append ops'.ArityOps [|countVars expr|]  } ) (getOpStruct ())
+                                     ArityOps = Array.append ops'.ArityOps [|countVars expr|]  } ) ops
 
 
 let rec exec : (int -> bool) -> Ops -> seq<unit> = fun f opStruct -> 
@@ -131,11 +132,10 @@ let rec exec : (int -> bool) -> Ops -> seq<unit> = fun f opStruct ->
         let matches' = matches opStruct exprs'
         printfn "%A" matches'
         yield ()
-        let opStruct' = updateOps (matches' |> Array.filter (fun (_, c, _) -> c > 0) |> Array.take 3 |> Array.map (fun (_, _, expr) -> expr)) opStruct
+        let opStruct' = updateOps (matches' |> Array.filter (fun (_, c, _) -> c > 0) |> Array.take 1 |> Array.map (fun (_, _, expr) -> expr)) opStruct
         setTimeout(120.0)
-        let (result, _, _, _, opExpr', _) = run numOfVars opStruct' f 3  1 numOfSamples (baseSample ())
-        let expr = opExpr' (Var "res") (freshVars 8)
-        let expr' = expr |> toBoolExpr' |> removeVars
+        let (result, _, _, _, opExpr', instrs') = run numOfVars opStruct' f 3  1 numOfSamples (baseSample ())
+        let expr' = compileInstrsToBoolExprs opStruct'.ArityOps instrs'
         printfn "%A" expr'
         yield ()
         let result' = verify numOfVars f (fun i -> let g = eval' opStruct'.Ops expr' in g (toBits' numOfVars i))
